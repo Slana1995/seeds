bdac1169119a45026ca8a420c3e8623cb936d074
AngelinaS <sycheva_95@mail.ru>
2015-03-14 08:50:08 +0300

    already done

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,15 +1,19 @@
 class ArticleController < ApplicationController
   def index
-  	@articles = Article.paginate(:page => params[:page], :per_page => 10)
+  	@articles = Article.paginate(:page => params[:page], :per_page => 10).order("created_at DESC")
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find(find_params)
     @tags = @article.tags
   end
-  def tags
-    @tags = @article.tags
-    @tag = Tag.find(params[:id])
-    @articles = @tag.articles.order('id DESC')
-  end
+  # def tags
+  #   @tags = @article.tags
+  #   @tag = Tag.find(params[:id])
+  #   @articles = @tag.articles.order('id DESC')
+  # end
+  private
+    def find_params
+      @article = Article.find(params[:id]) 
+    end
 end
